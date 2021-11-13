fe763e5478f02248ae209f5c5bd684afe45ae78f
jishan <you@example.com>
2021-11-13 17:21:45 +0530

    day9 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,18 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project1/models/catelog.dart';
 import 'package:flutter_project1/widget/drawer.dart';
+import 'package:flutter_project1/widget/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(5, (index) => CatelogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
-        title: Text("Catelog App",style: TextStyle(color: Colors.black),),
+        title: Text(
+          "Catelog App",
+          style: TextStyle(color: Colors.black),
+        ),
       ),
-      body: Center(
-        child: Container(
-          child: Text("welcome to 30 days of flutter"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
